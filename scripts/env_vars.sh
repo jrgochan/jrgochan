da5d8e5746e2e0c8d57abf2a7ff931efa293fe21
Jason G <jrgochan@Jasons-MacBook-Pro.local>
2024-08-19 12:45:39 -0600

    adding env_vars

---
+++ scripts/env_vars.sh
@@ -0,0 +1,8 @@
+# Define environment variables for the script
+REPO_URL_BASE="git@github.com"
+REPO_URL_PATH="jrgochan/jrgochan"
+REPO_URL_EXTENSION="git"
+CODE_DIR="$HOME/code"
+GITHUB_DIR="$CODE_DIR/github.com"
+CLONE_DIR="$GITHUB_DIR/$REPO_URL_PATH"
+
